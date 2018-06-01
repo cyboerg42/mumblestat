5f4b88caed39e0d44822a9f82e72445a16804d04
cyborg00222 <cyborg00222@users.noreply.github.com>
2018-06-01 15:05:40 +0200

    formating code

--- mstat.c
+++ mstat.c
@@ -3,7 +3,7 @@
 #include <stdlib.h>
 #include <arpa/inet.h>
 #include <sys/socket.h>
-#include <errno.h> 
+#include <errno.h>
 #include <limits.h>
 #include <sys/time.h>
 #include <time.h>
@@ -23,23 +23,24 @@ int timeout=1;
 
 int string_in(char *my_str, char *string_list[], size_t num_strings)
 {
-    for ( int i = 0; i < num_strings; i++ ) if (strcmp(my_str, string_list[i]) == 0 ) return i;
-    return 0;
+  for ( int i = 0; i < num_strings; i++ ) if (strcmp(my_str, string_list[i]) == 0 ) return i;
+  return 0;
 }
 
 void handleArguments(int argc, char *argv[]) {
-	int tmp = string_in("-p", argv, argc);
-	if (tmp > 0) {
-		char *p;
-		int num;
-		errno = 0;
-		long conv = strtol(argv[tmp+1], &p, 10);
-		if (!(errno != 0 || *p != '\0' || conv > INT_MAX)) {
-			num = conv;
-			PORT = num;
-		}
-	}
-	tmp = string_in("-s", argv, argc);
+  int tmp = string_in("-p", argv, argc);
+  if (tmp > 0) {
+    char *p;
+    int num;
+    errno = 0;
+    long conv = strtol(argv[tmp+1], &p, 10);
+    if (!(errno != 0 || *p != '\0' || conv > INT_MAX)) {
+      num = conv;
+      PORT = num;
+    }
+  }
+
+  tmp = string_in("-s", argv, argc);
   if (tmp > 0) {
     char *p;
     int num;
@@ -50,8 +51,9 @@ void handleArguments(int argc, char *argv[]) {
       S_TIME = num;
     }
   }
-	tmp = string_in("-c", argv, argc);
-	if (tmp > 0) {
+
+  tmp = string_in("-c", argv, argc);
+  if (tmp > 0) {
     char *p;
     int num;
     errno = 0;
@@ -61,10 +63,11 @@ void handleArguments(int argc, char *argv[]) {
       COUNT = num;
     }
   }
+
   tmp = string_in("-h", argv, argc);
   if (tmp > 0) {
-   	printf("where you are, there is no help. i'm sorry.\n");
-	  exit(0);
+    printf("where you are, there is no help. i'm sorry.\n");
+    exit(0);
   }
 }
 
@@ -89,54 +92,47 @@ int * decodePingResponse(char buf[]) {
 
 uint64_t get_posix_clock_time ()
 {
-    struct timespec ts;
-    if (clock_gettime (CLOCK_MONOTONIC, &ts) == 0) return (uint64_t) (ts.tv_sec * 1000000 + ts.tv_nsec / 1000);
-    else return 0;
+  struct timespec ts;
+  if (clock_gettime (CLOCK_MONOTONIC, &ts) == 0) return (uint64_t) (ts.tv_sec * 1000000 + ts.tv_nsec / 1000);
+  else return 0;
 }
 
 void die(char *s)
 {
-    perror(s);
-    exit(1);
+  perror(s);
+  exit(1);
 }
 
 
 int main(int argc, char *argv[])
 {
-    handleArguments(argc, argv);
-    struct sockaddr_in si_other;
-    int s, i, slen=sizeof(si_other);
-    char buf[BUFLEN];
-    char message[BUFLEN] = MUMBLE_PING;
-    if ( (s=socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP)) == -1)
-    {
-        die("socket");
-    }
-    memset((char *) &si_other, 0, sizeof(si_other));
-    si_other.sin_family = AF_INET;
-    si_other.sin_port = htons(PORT);
-    if (inet_aton(argv[1] , &si_other.sin_addr) == 0) 
-    {
-        fprintf(stderr, "inet_aton() failed\n");
-        exit(1);
-    }
-    while(1 && COUNT != 0)
-    {
-        uint64_t time1 = get_posix_clock_time();
-        if (sendto(s, message, 12 , 0 , (struct sockaddr *) &si_other, slen)==-1) {
-	        die("sendto()");
-        }
-        memset(buf,'\0', BUFLEN);
-        sleep(timeout);
-        if (recvfrom(s, buf, BUFLEN, MSG_DONTWAIT, (struct sockaddr *) &si_other, &slen) == -1) break;
-	      uint64_t time2 = get_posix_clock_time()-(1000000*timeout);
-	      float ping = ((time2 - time1)/1000.0);
-	      int *response = decodePingResponse(buf);
-	      printf("Users : %i/%i - %.2f ms\n", *(response + 0), *(response + 1), ping);
-	      COUNT--;
-        if (COUNT == 0) break;
-        sleep(S_TIME);
-    }
-    close(s);
-    return 0;
+  handleArguments(argc, argv);
+  struct sockaddr_in si_other;
+  int s, i, slen=sizeof(si_other);
+  char buf[BUFLEN];
+  char message[BUFLEN] = MUMBLE_PING;
+  if ( (s=socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP)) == -1) die("socket");
+  memset((char *) &si_other, 0, sizeof(si_other));
+  si_other.sin_family = AF_INET;
+  si_other.sin_port = htons(PORT);
+  if (inet_aton(argv[1] , &si_other.sin_addr) == 0) {
+    fprintf(stderr, "inet_aton() failed\n");
+    exit(1);
+  }
+  while(1 && COUNT != 0) {
+    uint64_t time1 = get_posix_clock_time();
+    if (sendto(s, message, 12 , 0 , (struct sockaddr *) &si_other, slen)==-1) die("sendto()");
+    memset(buf,'\0', BUFLEN);
+    sleep(timeout);
+    if (recvfrom(s, buf, BUFLEN, MSG_DONTWAIT, (struct sockaddr *) &si_other, &slen) == -1) break;
+    uint64_t time2 = get_posix_clock_time()-(1000000*timeout);
+    float ping = ((time2 - time1)/1000.0);
+    int *response = decodePingResponse(buf);
+    printf("Users : %i/%i - %.2f ms\n", *(response + 0), *(response + 1), ping);
+    COUNT--;
+    if (COUNT == 0) break;
+    sleep(S_TIME);
+  }
+  close(s);
+  return 0;
 }
